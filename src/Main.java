bcb601887e2d38d6bffd23701eaf0146e93eb123
Michael <michaelw.apple@icloud.com>
2023-04-18 09:53:59 +0200

    Initital Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        int num1 = 3;
+        int num2 = 5;
+
+        int sum = num1+num2;
+
+
+        System.out.println("Das Ergebnis lautet: " + sum);
+
+        String done = "U've done it! GZ!";
+
+        System.out.println(done);
+        }
+    }
